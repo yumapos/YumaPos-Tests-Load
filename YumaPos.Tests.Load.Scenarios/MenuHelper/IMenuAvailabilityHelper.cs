6935d38d463b6f73c80e253fa9582e66fa3d2aca
knaiv <ivan.k@yumasoft.com>
2016-11-01 18:38:33 +0300

    add new scenarios;

---
+++ YumaPos.Tests.Load.Scenarios/MenuHelper/IMenuAvailabilityHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using YumaPos.Shared.Core.MenuModels;
+
+namespace YumaPos.Tests.Load.Scenarios.MenuHelper
+{
+    internal interface IMenuAvailabilityHelper
+    {
+        IReadOnlyCollection<IMenuItem> GetAvailableMenuItems();
+    }
+}
---
+++ YumaPos.Tests.Load.Scenarios/MenuHelper/MenuAvailabilityHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using YumaPos.Shared.API.Models;
+using YumaPos.Shared.Core.MenuModels;
+using YumaPos.Shared.Core.Utils.Availability;
+
+namespace YumaPos.Tests.Load.Scenarios.MenuHelper
+{
+    internal sealed class MenuAvailabilityHelper : IMenuAvailabilityHelper
+    {
+        private readonly TerminalContext _context;
+
+        public MenuAvailabilityHelper(TerminalContext context)
+        {
+            _context = context;
+        }
+
+        public IReadOnlyCollection<IMenuItem> GetAvailableMenuItems()
+        {
+            var menuItems = _context.TreeMenu.MenuItems.Select(a => a.Value);
+
+            return menuItems.Where(menuItem =>
+                menuItem.Availabilities.Any() &&
+                CheckingAvailability.Check(menuItem.Availabilities.Select(Map), DateTime.Now) &&
+                menuItem.Price > 0
+                ).ToList();
+        }
+
+        private static AvailabilityDto Map(IAvailability availability)
+        {
+            return new AvailabilityDto
+            {
+                AvailabilityId = availability.AvailabilityId,
+                Days = availability.Days,
+                IsChecked = availability.IsChecked,
+                TimeFrom = availability.DaySecondsFrom,
+                TimeTo = availability.DaySecondsTo
+            };
+        }
+    }
+}
